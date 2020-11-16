13bec8bf63547caa10af284635173c6aa9ad3af6
Mikhail Kaminsky <kam.m.a@ya.ru>
2020-11-16 11:43:50 +0300

    Add style for checked item

--- src/components/Todos/TodoList/Item/Item.jsx
+++ src/components/Todos/TodoList/Item/Item.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import s from './Item.module.scss';
 
-const Item = ({ title }) => {
+const Item = ({ title, completed }) => {
   return (
     <li className={s.root}>
       <input className={s.checkbox} type="checkbox" />
-      <label className={s.label}>{title}</label>
+      <label className={cn(s.label, { [s.checked]: completed })}>{title}</label>
     </li>
   );
 };
 
 Item.propTypes = {
   title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
 };
 
 export default Item;
